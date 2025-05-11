6dca53d2b3459214eb6b2368b7a5d8faa525b318
RosticBogdan <bogdanrostic@gmail.com>
2025-05-11 17:47:53 +0000

    Remove Electron from the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
-import renderer from 'vite-plugin-electron-renderer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    electron([
-      {
-        entry: 'electron/main.cjs',
-        vite: {
-          build: {
-            outDir: 'dist-electron/main',
-          },
-        },
-      },
-      {
-        entry: 'electron/preload.cjs',
-        vite: {
-          build: {
-            outDir: 'dist-electron/preload',
-          },
-        },
-      },
-    ]),
-    renderer(),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
